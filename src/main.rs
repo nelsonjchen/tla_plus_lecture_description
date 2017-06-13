16ad3d69d703af7e5b335367033447654d344e79
Nelson Chen <crazysim@gmail.com>
2017-06-12 23:32:53 -0700

    Format

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@ extern crate time;
 use time::Duration;
 
 const DISCLAIMER: &str = "
-IMPORTANT NOTE!: This is an UNOFFICIAL and UNSANCTIONED reupload of the TLA+ lecture videos with some chapter markers in the video description on Lamport's site for ease of viewing on mobile devices, or other platforms and connection setups where YouTube is the most optimal \"format\". For the original files and original resources, please visit http://lamport.azurewebsites.net/video/videos.html.
+IMPORTANT NOTE!: This is an UNOFFICIAL and UNSANCTIONED reupload of \
+                          the TLA+ lecture videos with some chapter markers in the video \
+                          description on Lamport's site for ease of viewing on mobile devices, or \
+                          other platforms and connection setups where YouTube is the most optimal \
+                          \"format\". For the original files and original resources, please visit \
+                          http://lamport.azurewebsites.net/video/videos.html.
 ";
 
 struct Video<'a> {
@@ -18,19 +23,16 @@ struct Marker<'a> {
 }
 
 fn main() {
-    let videos = vec![
-        Video {
-            name: "Introduction to TLA+",
-            description: "Explains what TLA+ is and why you might want to use it.  It introduces the concept of a state machine.",
-            original_link: "http://lamport.azurewebsites.net/video/intro.html",
-            markers: vec![
-                Marker{
-                    name: "Prologue",
-                    timestamp: Duration::seconds(5),
-                }
-            ]
-        }
-    ];
+    let videos = vec![Video {
+                          name: "Introduction to TLA+",
+                          description: "Explains what TLA+ is and why you might want to use it.  \
+                                        It introduces the concept of a state machine.",
+                          original_link: "http://lamport.azurewebsites.net/video/intro.html",
+                          markers: vec![Marker {
+                                            name: "Prologue",
+                                            timestamp: Duration::seconds(5),
+                                        }],
+                      }];
 
     for video in videos {
         println!("===BEGIN COPY: Original Name: {}===", video.name);
@@ -40,9 +42,9 @@ fn main() {
         println!("\nContents\n");
         for marker in video.markers {
             println!("{} - {:02}:{:02}",
-             marker.name,
-             marker.timestamp.num_minutes(),
-             marker.timestamp.num_seconds());
+                     marker.name,
+                     marker.timestamp.num_minutes(),
+                     marker.timestamp.num_seconds());
         }
         println!("===END COPY: Original Name: {}===", video.name);
     }
